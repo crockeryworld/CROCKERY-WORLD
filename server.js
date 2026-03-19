468598a41fe9d2dbf7fc7b94b9d65ac616fe45da
crockeryworld <worldcrockery30@gmail.com>
2026-03-19 16:23:29 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -82,6 +82,9 @@ app.put("/update/:id", async (req, res) => {
 
 // ✅ IMPORTANT for Render
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
